1d60389a0a751f607b23c5546e1548632e909867
Dillon Powers <dillon.c.powers@gmail.com>
2018-11-19 14:43:56 -0500

    LIB-220 add initial envSetup.ps1

---
+++ Infrastructure/envSetup.ps1
@@ -0,0 +1,11 @@
+New-Module -Name EnvSetup -ScriptBlock {
+    # Install nuget
+    $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+    $targetNugetExe = ".\nuget.exe"
+    Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+    Set-Alias nuget $targetNugetExe -Scope Global -Verbose
+    
+    # Add DotFeed as source
+    nuget sources add -Name DotFeed -Source $env:DotFeedUri -username buildAgent -password $env:DotFeedPassword
+    nuget sources list
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Gists
+
+## Why
+
+Because organizations can't have gists and we need a place to put some publicly accessible code.
