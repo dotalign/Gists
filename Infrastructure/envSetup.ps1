14065e4f0e4e954074520a8d5ca94ce117a5e54d
Sarah Herr <sarah.herr@hotmail.com>
2019-03-13 15:22:57 -0400

    Update envSetup.ps1

--- Infrastructure/envSetup.ps1
+++ Infrastructure/envSetup.ps1
@@ -3,7 +3,7 @@ New-Module -Name EnvSetup -ScriptBlock {
     $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
     $targetNugetExe = ".\nuget.exe"
     Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
-    "============="
+    
     
     Set-Alias nuget $targetNugetExe -Scope Global -Verbose
     
