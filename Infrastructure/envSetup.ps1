c05adef184d0a5b1ae95c0ebf44a70f147be28b3
Sarah Herr <sarah.herr@hotmail.com>
2019-03-13 15:37:20 -0400

    Update envSetup.ps1

--- Infrastructure/envSetup.ps1
+++ Infrastructure/envSetup.ps1
@@ -3,7 +3,6 @@ New-Module -Name EnvSetup -ScriptBlock {
     $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
     $targetNugetExe = ".\nuget.exe"
     Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
-    
     Set-Alias nuget $targetNugetExe -Scope Global -Verbose
     
     # Add DotFeed as source
