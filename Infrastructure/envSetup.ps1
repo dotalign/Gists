ea9bdf0abcb8149f2d6be1ffc6ffb775f6c1d01d
Sarah Herr <sarah.herr@hotmail.com>
2019-03-14 10:53:59 -0400

    Update envSetup.ps1

--- Infrastructure/envSetup.ps1
+++ Infrastructure/envSetup.ps1
@@ -6,8 +6,8 @@ New-Module -Name EnvSetup -ScriptBlock {
     Set-Alias nuget $targetNugetExe -Verbose
     
     # Add DotFeed as source
-    Write-Host (nuget sources add -Name DotFeed -Source "https://dotalign.pkgs.visualstudio.com/_packaging/DotFeed/nuget/v3/index.json" -username buildAgent -password $env:BuildAgentDotFeedPassword -Verbosity Detailed) -ForegroundColor Cyan
-    Write-Host (nuget sources list -Name DotFeed) -ForegroundColor Yellow
-    
-    Write-Host (nuget list Dot. -Source DotFeed) -ForegroundColor Green
+    Write-Host (nuget sources add -Name DotFeed -Source "https://dotalign.pkgs.visualstudio.com/_packaging/DotFeed/nuget/v3/index.json" -username buildAgent -password $env:BuildAgentDotFeedPassword -Verbosity Detailed)
+    # Make sure we can access DotFeed
+    Write-Host (nuget sources list -Name DotFeed)
+    Write-Host (nuget list Dot. -Source DotFeed)
 }
