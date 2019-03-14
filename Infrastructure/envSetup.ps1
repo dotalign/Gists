8d78799a2dda6729e7187f02e6431cf43dcd5bb4
Sarah Herr <sarah.herr@hotmail.com>
2019-03-14 10:39:22 -0400

    Update envSetup.ps1

--- Infrastructure/envSetup.ps1
+++ Infrastructure/envSetup.ps1
@@ -3,15 +3,11 @@ New-Module -Name EnvSetup -ScriptBlock {
     $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
     $targetNugetExe = ".\nuget.exe"
     Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
-    
-    echo $env:DotFeedUri
-    
     Set-Alias nuget $targetNugetExe -Verbose
     
     # Add DotFeed as source
-    Write-Host "========== Adding feed now ==========="
-    
     Write-Host (nuget sources add -Name DotFeed -Source "https://dotalign.pkgs.visualstudio.com/_packaging/DotFeed/nuget/v3/index.json" -username buildAgent -password $env:BuildAgentDotFeedPassword -Verbosity Detailed)
-    Write-Host (nuget sources list -Name DotFeed)
-    Write-Host (nuget list -Source DotFeed -verbosity detailed)
+    Write-Output (nuget sources list -Name DotFeed)
+    
+    Write-Output (nuget list dot.cake -Name DotFeed)
 }
